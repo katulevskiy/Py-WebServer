6f458ffb2a9fa78092cb2541ae8e8847c683e2de
Daniel Katulevskiy <katulevskiy@gmail.com>
2023-04-29 23:17:41 -0700

    fix handle EINTR error

--- anti-zombie-SIGCHLD.py
+++ anti-zombie-SIGCHLD.py
@@ -1,18 +1,14 @@
+import errno
 import os
 import signal
 import socket
-import time
 
 SERVER_ADDRESS = (HOST, PORT) = '', 8888
-REQUEST_QUEUE_SIZE = 5
+REQUEST_QUEUE_SIZE = 1024
 
 
 def grim_reaper(signum, frame):
     pid, status = os.wait()
-    print(
-        'Child {pid} terminated with status {status}'
-        '\n'.format(pid=pid, status=status)
-    )
 
 
 def handle_request(client_connection):
@@ -24,8 +20,6 @@ HTTP/1.1 200 OK
 Hello, World!
 """
     client_connection.sendall(http_response)
-    # sleep to allow the parent to loop over to 'accept' and block there
-    time.sleep(3)
 
 
 def serve_forever():
@@ -38,7 +32,16 @@ def serve_forever():
     signal.signal(signal.SIGCHLD, grim_reaper)
 
     while True:
-        client_connection, client_address = listen_socket.accept()
+        try:
+            client_connection, client_address = listen_socket.accept()
+        except IOError as e:
+            code, msg = e.args
+            # restart 'accept' if it was interrupted
+            if code == errno.EINTR:
+                continue
+            else:
+                raise
+
         pid = os.fork()
         if pid == 0:  # child
             listen_socket.close()  # close child copy
@@ -46,8 +49,8 @@ def serve_forever():
             client_connection.close()
             os._exit(0)
         else:  # parent
-            client_connection.close()
+            client_connection.close()  # close parent copy and loop over
+
 
 if __name__ == '__main__':
     serve_forever()
-
